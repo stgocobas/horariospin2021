3b097df56c0b7db3bcb3ed10259388d8642098dd
stgocobas <88646371+stgocobas@users.noreply.github.com>
2021-08-09 01:38:23 -0400

    Add files via upload

---
+++ horario_tabla.php
@@ -0,0 +1,535 @@
+<!DOCTYPE html>
+
+<!-- Inicializar SQL y PHP-->
+<?php
+session_start();
+$host = "localhost"; /* Host name */
+$user = "root"; /* User */
+$password = ""; /* Password */
+$dbname = "colegio"; /* Database name */
+
+$con = mysqli_connect($host, $user, $password,$dbname);
+if (!$con) { die("Conexión fallida: " . mysqli_connect_error()); }
+?>
+
+<html>
+<!-- Referencias -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" id="estilo1" href="imagenes.css" type="text/css" media="all">
+<link rel="stylesheet" id="estilo1" href="texto.css" type="text/css" media="all">
+<link rel="stylesheet" id="estilo1" href="barra_menu.css" type="text/css" media="all">
+<link rel="stylesheet" id="estilo1" href="noticias.css" type="text/css" media="all">
+<link rel="stylesheet" id="estilo1" href="horario.css" type="text/css" media="all">
+
+<!-- button onclick="exportTableToCSV('members.csv')">Export HTML Table To CSV File</button -->
+
+<table id="data" style="width:100%">
+  <tr>
+	<th>Módulo</th>
+    <th>Lunes</th>
+    <th>Martes</th>
+    <th>Miércoles</th>
+	<th>Jueves</th>
+	<th>Viernes</th>
+  </tr>
+
+<?php
+
+function GetColor($materia)
+{
+	switch($materia)
+	{
+		default:
+		{
+			return "<td style=\"background-color:white\">";
+		}
+		break;
+		case "CHOQUE":
+		{
+			return "<td style=\"background-color:#e03d3d\">";
+		}
+		case "NADA":
+		{
+			return "<td style=\"background-color:lightgrey\">";
+		}
+		case "ELT121":
+		{
+			return "<td style=\"background-color:#fff2cc\">";
+		}
+		break;
+		case "ELT122":
+		{
+			return "<td style=\"background-color:#d9d2e9\">";
+		}
+		break;
+		case "ELT123-1":
+		{
+			return "<td style=\"background-color:#d0e0e3\">";
+		}
+		break;
+		case "ELT123-2":
+		{
+			return "<td style=\"background-color:#d0e0e3\">";
+		}
+		break;
+		case "ELT123-3":
+		{
+			return "<td style=\"background-color:#d0e0e3\">";
+		}
+		break;
+		case "ELT124-1":
+		{
+			return "<td style=\"background-color:#f4cccc\">";
+		}
+		break;
+		case "ELT124-2":
+		{
+			return "<td style=\"background-color:#f4cccc\">";
+		}
+		break;
+		case "ELT124-3":
+		{
+			return "<td style=\"background-color:#f4cccc\">";
+		}
+		break;
+		case "ELT125-1":
+		{
+			return "<td style=\"background-color:#9fc5e8\">";
+		}
+		break;
+		case "ELT125-2":
+		{
+			return "<td style=\"background-color:#9fc5e8\">";
+		}
+		break;
+		case "ELT125-3":
+		{
+			return "<td style=\"background-color:#9fc5e8\">";
+		}
+		break;
+		case "ELT126-1":
+		{
+			return "<td style=\"background-color:#fce5cd\">";
+		}
+		break;
+		case "ELT126-2":
+		{
+			return "<td style=\"background-color:#fce5cd\">";
+		}
+		break;
+		case "ELT126-3":
+		{
+			return "<td style=\"background-color:#fce5cd\">";
+		}
+		break;
+	}
+}
+
+function GetTeacherName($materia)
+{
+	switch($materia)
+	{
+		case "NADA":
+		{
+			return "<br>";
+		}
+		case "ELT121":
+		{
+			return "Fundamentos Sociologicos (S1 - Fraño Paukner)<br>";
+		}
+		break;
+		case "ELT122":
+		{
+			return "Fundamentos Psicologicos (S1 - Claudia Mendez)<br>";
+		}
+		break;
+		case "ELT123-1":
+		{
+			return "Competencias Comunicativas II (S1 - Juan Carlos Cardenas)<br>";
+		}
+		break;
+		case "ELT123-2":
+		{
+			return "Competencias Comunicativas II (S2 - Alejandro Ayala)<br>";
+		}
+		break;
+		case "ELT123-3":
+		{
+			return "Competencias Comunicativas II (S3 - Alejandro Ayala)<br>";
+		}
+		break;
+		case "ELT124-1":
+		{
+			return "Gramatica Contextualizada II (S1 - Gloria Kanelos)<br>";
+		}
+		break;
+		case "ELT124-2":
+		{
+			return "Gramatica Contextualizada II (S2 - Sarka Baglova)<br>";
+		}
+		break;
+		case "ELT124-3":
+		{
+			return "Gramatica Contextualizada II (S3 - Barbara Tapia)<br>";
+		}
+		break;
+		case "ELT125-1":
+		{
+			return "Fonetica II (S1 - Bruno Ramirez)<br>";
+		}
+		break;
+		case "ELT125-2":
+		{
+			return "Fonetica II (S2 - Bruno Ramirez)<br>";
+		}
+		break;
+		case "ELT125-3":
+		{
+			return "Fonetica II (S3 - Alejandro Ayala)<br>";
+		}
+		break;
+		case "ELT126-1":
+		{
+			return "Introduccion al Aprendizaje (S1 - Rodrigo Santos)<br>";
+		}
+		break;
+		case "ELT126-2":
+		{
+			return "Introduccion al Aprendizaje (S2 - Evelyn Diaz)<br>";
+		}
+		break;
+		case "ELT126-3":
+		{
+			return "Introduccion al Aprendizaje (S3 - Tamara Salazar)<br>";
+		}
+		break;
+	}
+}
+
+
+// ELT121 = FUND SOCIOLOGICOS
+// ELT122 = FUND PSICOLOGICOS
+// ELT123 = COMPETENCIAS
+// ELT124 = GRAMATICA
+// ELT125 = FONETICA
+// ELT126 = INTRODUCCION A LA ENSEÑANZA
+
+$lunes = ["ELT122", "ELT122", "", "", "NADA", "", "", "", "", ""];
+$martes = ["", "", "ELT121", "ELT121", "NADA", "", "", "", "", ""];
+$miercoles = ["", "", "ELT121", "ELT122", "NADA", "", "", "", "", ""];
+$jueves = ["", "", "", "", "NADA", "", "", "", "", ""];
+$viernes = ["", "", "", "", "NADA", "", "", "", "", ""];
+
+// COMPETENCIAS ELT123
+if($_SESSION['ELT123'] == 1)
+{
+	if($lunes[5] != "") $lunes[5] .= ",";
+	if($lunes[6] != "") $lunes[6] .= ",";
+	$lunes[5] .= "ELT123-1";
+	$lunes[6] .= "ELT123-1";
+	
+	if($martes[7] != "") $martes[7] .= ",";
+	if($martes[8] != "") $martes[8] .= ",";
+	$martes[7] .= "ELT123-1";
+	$martes[8] .= "ELT123-1";
+	
+	if($jueves[7] != "") $jueves[7] .= ",";
+	if($jueves[8] != "") $jueves[8] .= ",";
+	$jueves[7] .= "ELT123-1";
+	$jueves[8] .= "ELT123-1";
+	
+	if($viernes[2] != "") $viernes[2] .= ",";
+	if($viernes[3] != "") $viernes[3] .= ",";
+	$viernes[2] .= "ELT123-1";
+	$viernes[3] .= "ELT123-1";
+}
+else if($_SESSION['ELT123'] == 2)
+{
+	if($lunes[5] != "") $lunes[5] .= ",";
+	if($lunes[6] != "") $lunes[6] .= ",";
+	$lunes[5] .= "ELT123-2";
+	$lunes[6] .= "ELT123-2";
+	
+	if($martes[7] != "") $martes[7] .= ",";
+	if($martes[8] != "") $martes[8] .= ",";
+	$martes[7] .= "ELT123-2";
+	$martes[8] .= "ELT123-2";
+	
+	if($jueves[7] != "") $jueves[7] .= ",";
+	if($jueves[8] != "") $jueves[8] .= ",";
+	$jueves[7] .= "ELT123-2";
+	$jueves[8] .= "ELT123-2";
+	
+	if($viernes[2] != "") $viernes[2] .= ",";
+	if($viernes[3] != "") $viernes[3] .= ",";
+	$viernes[2] .= "ELT123-2";
+	$viernes[3] .= "ELT123-2";
+}
+else if($_SESSION['ELT123'] == 3)
+{
+	if($lunes[7] != "") $lunes[7] .= ",";
+	if($lunes[8] != "") $lunes[8] .= ",";
+	$lunes[7] .= "ELT123-3";
+	$lunes[8] .= "ELT123-3";
+	
+	if($martes[5] != "") $martes[5] .= ",";
+	if($martes[6] != "") $martes[6] .= ",";
+	$martes[5] .= "ELT123-3";
+	$martes[6] .= "ELT123-3";
+	
+	if($jueves[7] != "") $jueves[7] .= ",";
+	if($jueves[8] != "") $jueves[8] .= ",";
+	$jueves[7] .= "ELT123-3";
+	$jueves[8] .= "ELT123-3";
+	
+	if($viernes[0] != "") $viernes[0] .= ",";
+	if($viernes[1] != "") $viernes[1] .= ",";
+	$viernes[0] .= "ELT123-3";
+	$viernes[1] .= "ELT123-3";
+}
+
+// GRAMATICA ELT124
+if($_SESSION['ELT124'] == 1)
+{
+	if($miercoles[0] != "") $miercoles[0] .= ",";
+	if($miercoles[1] != "") $miercoles[1] .= ",";
+	$miercoles[0] .= "ELT124-1";
+	$miercoles[1] .= "ELT124-1";
+	
+	if($jueves[2] != "") $jueves[2] .= ",";
+	if($jueves[3] != "") $jueves[3] .= ",";
+	$jueves[2] .= "ELT124-1";
+	$jueves[3] .= "ELT124-1";
+}
+else if($_SESSION['ELT124'] == 2)
+{
+	if($miercoles[0] != "") $miercoles[0] .= ",";
+	if($miercoles[1] != "") $miercoles[1] .= ",";
+	$miercoles[0] .= "ELT124-2";
+	$miercoles[1] .= "ELT124-2";
+	
+	if($jueves[2] != "") $jueves[2] .= ",";
+	if($jueves[3] != "") $jueves[3] .= ",";
+	$jueves[2] .= "ELT124-2";
+	$jueves[3] .= "ELT124-2";
+}
+else if($_SESSION['ELT124'] == 3)
+{
+	if($miercoles[0] != "") $miercoles[0] .= ",";
+	if($miercoles[1] != "") $miercoles[1] .= ",";
+	$miercoles[0] .= "ELT124-3";
+	$miercoles[1] .= "ELT124-3";
+	
+	if($jueves[2] != "") $jueves[2] .= ",";
+	if($jueves[3] != "") $jueves[3] .= ",";
+	$jueves[2] .= "ELT124-3";
+	$jueves[3] .= "ELT124-3";
+}
+
+// FONETICA ELT125
+if($_SESSION['ELT125'] == 1)
+{
+	if($martes[0] != "") $martes[0] .= ",";
+	if($martes[1] != "") $martes[1] .= ",";
+	$martes[0] .= "ELT125-1";
+	$martes[1] .= "ELT125-1";
+	
+	if($jueves[0] != "") $jueves[0] .= ",";
+	if($jueves[1] != "") $jueves[1] .= ",";
+	$jueves[0] .= "ELT125-1";
+	$jueves[1] .= "ELT125-1";
+}
+else if($_SESSION['ELT125'] == 2)
+{
+	if($lunes[7] != "") $lunes[7] .= ",";
+	if($lunes[8] != "") $lunes[8] .= ",";
+	$lunes[7] .= "ELT125-2";
+	$lunes[8] .= "ELT125-2";
+	
+	if($martes[5] != "") $martes[5] .= ",";
+	if($martes[6] != "") $martes[6] .= ",";
+	$martes[5] .= "ELT125-2";
+	$martes[6] .= "ELT125-2";
+}
+else if($_SESSION['ELT125'] == 3)
+{
+	if($martes[0] != "") $martes[0] .= ",";
+	if($martes[1] != "") $martes[1] .= ",";
+	$martes[0] .= "ELT125-3";
+	$martes[1] .= "ELT125-3";
+	
+	if($jueves[0] != "") $jueves[0] .= ",";
+	if($jueves[1] != "") $jueves[1] .= ",";
+	$jueves[0] .= "ELT125-3";
+	$jueves[1] .= "ELT125-3";
+}
+
+// INTRO. ENSEÑANZA ELT126
+if($_SESSION['ELT126'] == 1)
+{
+	if($miercoles[7] != "") $miercoles[7] .= ",";
+	if($miercoles[8] != "") $miercoles[8] .= ",";
+	$miercoles[7] .= "ELT126-1";
+	$miercoles[8] .= "ELT126-1";
+}
+else if($_SESSION['ELT126'] == 2)
+{
+	if($miercoles[7] != "") $miercoles[7] .= ",";
+	if($miercoles[8] != "") $miercoles[8] .= ",";
+	$miercoles[7] .= "ELT126-2";
+	$miercoles[8] .= "ELT126-2";
+}
+else if($_SESSION['ELT126'] == 3)
+{
+	if($viernes[2] != "") $viernes[2] .= ",";
+	if($viernes[3] != "") $viernes[3] .= ",";
+	$viernes[2] .= "ELT126-3";
+	$viernes[3] .= "ELT126-3";
+}
+
+echo("<h1>Generador de Horario PIN 2021</h1>");
+echo("<tr>");
+
+// Módulo 1
+
+for($modulo = 0; $modulo < 10; $modulo++)
+{
+	$modulostr = "";
+	
+	switch($modulo)
+	{
+		case 0:
+			$modulostr = "<td>Modulo 1 | 08:30 - 09:30</td>";
+		break;
+		case 1:
+			$modulostr = "<td>Modulo 2 | 09:35 - 10:35</td>";
+		break;
+		case 2:
+			$modulostr = "<td>Modulo 3 | 10:50 - 11:50</td>";
+		break;
+		case 3:
+			$modulostr = "<td>Modulo 4 | 11:55 - 12:55</td>";
+		break;
+		case 4:
+			$modulostr = "<td>Modulo 5 | 13:10 - 14:10</td>";
+		break;
+		case 5:
+			$modulostr = "<td>Modulo 6 | 14:30 - 15:30</td>";
+		break;
+		case 6:
+			$modulostr = "<td>Modulo 7 | 15:35 - 16:35</td>";
+		break;
+		case 7:
+			$modulostr = "<td>Modulo 8 | 16:50 - 17:50</td>";
+		break;
+		case 8:
+			$modulostr = "<td>Modulo 9 | 18:55 - 19:55</td>";
+		break;
+		case 9:
+			$modulostr = "<td>Modulo 10 | 19:10 - 20:10</td>";
+		break;
+	}
+	
+	echo($modulostr);
+	
+	for($dia = 0; $dia < 5; $dia++)	
+	{
+		$ramodia = [];
+		
+		switch($dia)
+		{
+			case 0:
+				$ramodia = $lunes[$modulo];
+			break;
+			case 1:
+				$ramodia = $martes[$modulo];
+			break;
+			case 2:
+				$ramodia = $miercoles[$modulo];
+			break;
+			case 3:
+				$ramodia = $jueves[$modulo];
+			break;
+			case 4:
+				$ramodia = $viernes[$modulo];
+			break;
+		}
+		
+		$ramo = explode(",", $ramodia);
+		$str = "";
+		
+		if(count($ramo) == 1)
+		{
+			$str = GetColor($ramo[0]);
+		}
+		else
+		{
+			$str = GetColor("CHOQUE");
+		}
+		
+		for($x = 0; $x < count($ramo); $x++) $str = $str . GetTeacherName($ramo[$x]) . "<br>";
+		
+		$str = $str . "</td>";
+		echo($str);	
+	}
+	
+	echo("<tr>");
+}
+
+
+
+// ELT121 = FUND SOCIOLOGICOS
+// ELT122 = FUND PSICOLOGICOS
+// ELT123 = COMPETENCIAS
+// ELT124 = GRAMATICA
+// ELT125 = FONETICA
+// ELT126 = INTRODUCCION A LA ENSEÑANZA
+?>
+</table> 
+
+<script>
+function downloadCSV(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV file
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // Create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Hide download link
+    downloadLink.style.display = "none";
+
+    // Add the link to DOM
+    document.body.appendChild(downloadLink);
+
+    // Click download link
+    downloadLink.click();
+}
+
+function exportTableToCSV(filename) {
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+    
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+        
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+        csv.push(row.join("|"));        
+    }
+
+    // Download CSV file
+    downloadCSV(csv.join("n"), filename);
+}
+</script>
+
+<!-- button onclick="exportTableToCSV('members.csv')">Export HTML Table To CSV File</button>
